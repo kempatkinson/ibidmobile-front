ff981b90ca6b3b1934d407c535bf4748f946ec32
Kemp Atkinson <kempatkinson@gmail.com>
2020-05-18 15:13:01 -0500

    init

---
+++ client/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // The URL where the .NET Core app will be listening.
+    // Specific port depends on whether IISExpress/Kestrel and HTTP/HTTPS are used
+    devServer: {
+      proxy: 'https://localhost:5001'
+    },
+    publicPath: '/ibid_mobile/'
+
+  }
